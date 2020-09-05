b5c6dc83b8884022ef071aee8bf50da7fca271a6
fridayeveryday <p7ll7p@gmail.com>
2020-09-05 21:44:01 +0400

    почти

---
+++ src/Arithmetic.java
@@ -0,0 +1,279 @@
+import java.awt.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+class OperationPriority {
+    public int lvlPrior;
+    public ArrayList<String> operations;
+
+    public OperationPriority(int lvlPrior, ArrayList<String> operations) {
+        this.lvlPrior = lvlPrior;
+        this.operations = operations;
+    }
+}
+
+public class Arithmetic {
+
+    public OperationPriority firstPriorLvl = new OperationPriority(1, new ArrayList<>(Arrays.asList("(")));
+    public OperationPriority secondPriorLvl = new OperationPriority(2, new ArrayList<>(Arrays.asList("+", "-")));
+    public OperationPriority thirdPriorLvl = new OperationPriority(3, new ArrayList<>(Arrays.asList("*", "/")));
+    public OperationPriority fourthPriorLvl = new OperationPriority(4, new ArrayList<>(Arrays.asList("^")));
+    public OperationPriority fifthPriorLvl = new OperationPriority(5, new ArrayList<>(Arrays.asList("ln", "sin")));
+    public OperationPriority[] priorities = {firstPriorLvl, secondPriorLvl, thirdPriorLvl, fourthPriorLvl, fifthPriorLvl};
+
+    public ArrayList<String> operations = new ArrayList<>();
+
+    public String anyNumberInRegex = "[-]?\\d+[.,]?\\d*";
+    public String anyLettersInRegex = "[a-zA-Z]+";
+
+    public Arithmetic() {
+        for (OperationPriority op : priorities) {
+            operations.addAll(op.operations);
+        }
+        operations.addAll(Arrays.asList(")"));
+    }
+
+    public Number solveMathExpression(String arithmeticExpression) {
+        arithmeticExpression = arithmeticExpression.replaceAll("\\s+", "");
+        ArrayList<String> expressionComponents = parseExpression(arithmeticExpression);
+        if (!checkIsCorrectExpr(expressionComponents)) {
+            System.out.println("Incorrect format of expression. Please recheck it.");
+            return null;
+        }
+        ArrayList<String> elemInRPN = makeRPN(expressionComponents);
+        var res = solveExpressionInRPN(elemInRPN);
+        return res;
+    }
+
+    public boolean checkIsCorrectExpr(ArrayList<String> elemsOfExpr) {
+        for (String elem : elemsOfExpr) {
+            if (!operations.contains(elem)) {
+                try {
+                    Double.parseDouble(elem);
+                } catch (NumberFormatException exc) {
+                    return false;
+                }
+            }
+        }
+        int numOfBraces = 0;
+        for (String elem : elemsOfExpr) {
+            if (elem.equals("("))
+                numOfBraces++;
+            else if (elem.equals(")"))
+                numOfBraces--;
+        }
+        if (numOfBraces != 0) {
+            System.out.println("The number of opening and closing curly brackets does not match");
+            return false;
+        }
+
+        return true;
+    }
+
+    public Number solveExpressionInRPN(ArrayList<String> elemInRPN) {
+        Stack<Double> stack4Solving = new Stack<>();
+        for (String elem : elemInRPN) {
+            if (elem.matches(anyNumberInRegex)) {
+                stack4Solving.push(Double.parseDouble(elem));
+            } else if (fifthPriorLvl.operations.contains(elem)) {
+                double lastValInStack = stack4Solving.pop();
+                double resOfFun = 0;
+                switch (elem) {
+                    case "ln":
+                        if (lastValInStack > 0)
+                            resOfFun = Math.log(lastValInStack);
+                        else {
+                            System.out.println("The argument of the ln function is not in the range of acceptable values");
+                            return null;
+                        }
+                        break;
+                    case "sin":
+                        resOfFun = Math.sin(lastValInStack);
+                        break;
+                }
+                stack4Solving.push(resOfFun);
+            } else {
+                double secondNum = stack4Solving.pop();
+                double firstNum = stack4Solving.pop();
+                double res = 0;
+                switch (elem) {
+                    case "+":
+                        res = firstNum + secondNum;
+                        break;
+                    case "-":
+                        res = firstNum - secondNum;
+                        break;
+                    case "/":
+                        if (secondNum != 0)
+                            res = firstNum / secondNum;
+                        else {
+                            System.out.println("Division by zero.");
+                            return null;
+                        }
+                        break;
+
+                    case "*":
+                        res = firstNum * secondNum;
+                        break;
+                    case "^":
+                        res = Math.pow(firstNum, secondNum);
+                        break;
+                }
+                stack4Solving.push(res);
+            }
+        }
+        return stack4Solving.pop();
+    }
+
+    public ArrayList<String> makeRPN(ArrayList<String> partsOfExpression) {
+        ArrayList<String> revPolNot = new ArrayList<>();
+        Stack<String> stackOfOper = new Stack<String>();
+        for (String elem : partsOfExpression) {
+            // if elem is a number than add it to the revPolNot
+            if (elem.matches(anyNumberInRegex + "|" + anyLettersInRegex) && !fifthPriorLvl.operations.contains(elem)) {
+                revPolNot.add(elem);
+            } else {
+                if ((stackOfOper.empty() || elem.equals("(")) && !elem.equals(")")) {
+                    stackOfOper.push(elem);
+                } else if (elem.equals(")")) {
+                    while (true) {
+                        if (stackOfOper.lastElement().equals("(")) {
+                            stackOfOper.pop();
+                            if (!stackOfOper.isEmpty() && (fifthPriorLvl.operations.contains(stackOfOper.lastElement()))) {
+                                revPolNot.add(stackOfOper.pop());
+                            }
+                            break;
+                        }
+                        revPolNot.add(stackOfOper.pop());
+                    }
+                } else {
+                    if (exprPriorLvlIsTopPrior(elem, stackOfOper.lastElement())) {
+                        stackOfOper.push(elem);
+                    } else {
+                        while (!stackOfOper.isEmpty()) {
+                            revPolNot.add(stackOfOper.pop());
+                        }
+                        stackOfOper.push(elem);
+                    }
+                }
+            }
+        }
+        // make the stack empty
+        while (!stackOfOper.isEmpty()) {
+            revPolNot.add(stackOfOper.pop());
+        }
+        return revPolNot;
+    }
+
+    public boolean exprPriorLvlIsTopPrior(String expressionOperation, String stackOperation) {
+        int exprOperPrior = 0;
+        int stackOperPrior = 0;
+        for (OperationPriority op : priorities) {
+            if (op.operations.contains(expressionOperation))
+                exprOperPrior = op.lvlPrior;
+        }
+        for (OperationPriority op : priorities) {
+            if (op.operations.contains(stackOperation))
+                stackOperPrior = op.lvlPrior;
+        }
+        return exprOperPrior > stackOperPrior;
+    }
+
+    public ArrayList<String> parseExpression(String expression) {
+
+        char[] expressionByChar = expression.toCharArray();
+        ArrayList<String> partsOfExpression = new ArrayList<>();
+        StringBuilder operandOrFun = new StringBuilder("");
+
+        // number of braces what is needed to add
+        int numOfBracesAfterMinus = 0;
+        // show necessity to add braces after an opening brace was added
+        char s;
+        Stack<String> stackOfBracesAndMinuses = new Stack<String>();
+
+        for (int i = 0; i < expressionByChar.length; i++) {
+            s = expressionByChar[i];
+
+            // if s is an operation
+            if (operations.contains(Character.toString(s))) {
+                if (operandOrFun.length() != 0) {
+                    partsOfExpression.add(operandOrFun.toString());
+                    operandOrFun.setLength(0);
+
+                    if (numOfBracesAfterMinus > 0 && s != '(') {
+                        numOfBracesAfterMinus--;
+                        partsOfExpression.add(")");
+                    }
+                }
+
+                if (s == '-' && (expressionByChar[i + 1] == '('
+                        || expressionByChar[i + 1] == 'l'
+                        || expressionByChar[i + 1] == 's'
+                        || expressionByChar[i + 1] == 'c')) {
+//                    numOfBracesAfterMinus++;
+                    stackOfBracesAndMinuses.push(Character.toString(s));
+                    stackOfBracesAndMinuses.push(Character.toString(expressionByChar[i + 1]));
+                    String lastELemInPOfExp = "";
+                    if (!partsOfExpression.isEmpty())
+                        lastELemInPOfExp = partsOfExpression.get(partsOfExpression.size() - 1);
+                    if (lastELemInPOfExp.matches(anyNumberInRegex) || lastELemInPOfExp.equals(")")) {
+                        partsOfExpression.add("+");
+                    }
+                    partsOfExpression.add("(");
+                    partsOfExpression.add("-1");
+                    partsOfExpression.add("*");
+                    continue;
+                } else if (s == ')') {
+                    if (!stackOfBracesAndMinuses.empty())
+                        stackOfBracesAndMinuses.pop();
+                    String lastElem = "";
+                    if (!stackOfBracesAndMinuses.empty())
+                        lastElem = stackOfBracesAndMinuses.pop();
+                    if (lastElem.equals("-")) {
+                        // if operandOrFun is a string like "ln", "sin" or not one digit number
+                        if (operandOrFun.length() != 0) {
+                            partsOfExpression.add(operandOrFun.toString());
+                            operandOrFun.setLength(0);
+
+                            if (numOfBracesAfterMinus > 0 && s != '(') {
+                                numOfBracesAfterMinus--;
+                                partsOfExpression.add(")");
+                            }
+                        }
+                        partsOfExpression.add(")");
+
+                    }
+                }
+
+                if (s == '-') {
+                    String lastELemInPOfExp = "";
+                    if (!partsOfExpression.isEmpty())
+                        lastELemInPOfExp = partsOfExpression.get(partsOfExpression.size() - 1);
+                    if (lastELemInPOfExp.matches(anyNumberInRegex) || lastELemInPOfExp.equals(")")) {
+                        partsOfExpression.add("+");
+                    }
+                    numOfBracesAfterMinus++;
+                    partsOfExpression.add("(");
+                    partsOfExpression.add("-1");
+                    partsOfExpression.add("*");
+                } else {
+                    partsOfExpression.add(Character.toString(s));
+                }
+            } else {
+                operandOrFun.append(s);
+            }
+        }
+        if (operandOrFun.length() != 0) {
+            partsOfExpression.add(operandOrFun.toString());
+            operandOrFun.setLength(0);
+        }
+
+        while (numOfBracesAfterMinus > 0) {
+            partsOfExpression.add(")");
+            numOfBracesAfterMinus--;
+        }
+
+        return partsOfExpression;
+    }
+}
---
+++ src/Script.java
@@ -0,0 +1,233 @@
+import java.io.*;
+import java.sql.Array;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Script {
+
+
+    public static String path = "C:\\Temp\\ALM4.txt";
+    public static Scanner scanner;
+    private static Arithmetic arithmetic;
+
+    ArrayList<String> lexemesType = new ArrayList<>(Arrays.asList("num", "str", "if", "else", "in", "out"));
+    public static ArrayList<Lexeme> lexemes = new ArrayList<>();
+
+    public static void main(String[] args) {
+//        System.out.println(TypeLexemes.num);
+        arithmetic = new Arithmetic();
+        openFile(path);
+        int numberOfLine = 1;
+        while (scanner.hasNext()) {
+            String oneLine = readOneLine(scanner);
+            Lexeme currentLexeme = parseLine(oneLine);
+            lexemes.add(currentLexeme);
+            TypeLexemes typeCurrentLexeme = currentLexeme.type;
+            if (typeCurrentLexeme == TypeLexemes.num) {
+                if (!prepareNum(currentLexeme)) {
+                    System.out.printf("Error at %d line", numberOfLine);
+                    return;
+                }
+            } else if (typeCurrentLexeme == TypeLexemes.in) {
+                if (!prepareInput(currentLexeme)) {
+                    System.out.printf("Error at %d line", numberOfLine);
+                    return;
+                }
+            } else if (typeCurrentLexeme == TypeLexemes.out) {
+                prepareOutput(currentLexeme);
+            }
+
+        }
+//        lexemes.add(parseLine(readOneLine(scanner)));
+//        lexemes.add(parseLine(readOneLine(scanner)));
+//        lexemes.add(parseLine(readOneLine(scanner)));
+//        System.out.println(lexemes.get(0).content);
+//
+//        prepareNum(lexemes.get(0));
+//        prepareNum(lexemes.get(1));
+//        prepareNum(lexemes.get(2));
+//        ArrayList<String> codeLines = new ArrayList<>();
+//        while (scanner.hasNext()) {
+////            String oneLine = readCode(scanner);
+//
+//            System.out.println(readCode(scanner));
+//        }
+
+
+    }
+
+    public static void prepareOutput(Lexeme lexeme) {
+        String lexemeContent = lexeme.content;
+        if (lexemeContent.contains("\"")) {
+            System.out.println(lexemeContent.replaceAll("\"", ""));
+        } else if (lexemeContent.matches("[-]?\\d+[.,]?\\d*")) {
+            System.out.println(lexemeContent);
+        } else {
+            prepareNum(lexeme);
+            System.out.println(lexeme.content);
+        }
+    }
+
+    public static boolean prepareInput(Lexeme lexeme) {
+        if (lexeme.content.contains(" ")) {
+            return false;
+        }
+        Scanner scanner = new Scanner(System.in);
+        String value = scanner.nextLine();
+        TypeLexemes tL;
+        try {
+            value = String.valueOf(Double.parseDouble(value));
+            tL = TypeLexemes.num;
+        } catch (Exception ignored) {
+            tL = TypeLexemes.str;
+        }
+        for (int i = 0; i < lexemes.size(); i++) {
+            if (lexemes.get(i).name.equals(lexeme.content)) {
+                lexemes.set(i, new Lexeme(tL, lexeme.name, value));
+                return true;
+            }
+        }
+        lexemes.add(new Lexeme(tL, lexeme.name, value));
+        return true;
+    }
+//
+//    public static boolean prepareCondition(Lexeme lexeme) {
+//
+//    }
+
+    public static boolean prepareNum(Lexeme lexeme) {
+        Pattern p = Pattern.compile("[a-zA-Z]+");
+        String candidate = lexeme.content;
+        Matcher m = p.matcher(candidate);
+        if (m.find()) {
+            lexeme.content = substituteLexemes2Expression(lexeme);
+        }
+        p = Pattern.compile("[-+*/^]");
+        candidate = lexeme.content;
+        m = p.matcher(candidate);
+        if (m.find()) {
+            Double res = (Double) arithmetic.solveMathExpression(lexeme.content);
+            if (res != null) {
+                lexeme.content = res.toString();
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static String substituteLexemes2Expression(Lexeme lexeme) {
+        ArrayList<String> elemOfContents = new ArrayList<>();
+        StringBuilder operands = new StringBuilder();
+        for (char ch : lexeme.content.toCharArray()) {
+            if (arithmetic.operations.contains(Character.toString(ch))) {
+                if (operands.length() != 0) {
+                    elemOfContents.add(operands.toString());
+                    operands.setLength(0);
+                }
+                elemOfContents.add(Character.toString(ch));
+            } else {
+                operands.append(ch);
+            }
+        }
+        if (operands.length() != 0) {
+            elemOfContents.add(operands.toString());
+            operands.setLength(0);
+        }
+        for (int i = 0; i < lexemes.size() - 1; i++) {
+            Lexeme elem = lexemes.get(i);
+            for (int j = 0; j < elemOfContents.size(); j++) {
+                String elemofCon = elemOfContents.get(j);
+                if (elem.name.equals(elemofCon)) {
+                    elemOfContents.set(j, elem.content);
+                    break;
+                }
+            }
+
+        }
+        StringBuilder strContent = new StringBuilder();
+        for (String str : elemOfContents) {
+            strContent.append(str);
+        }
+        return strContent.toString();
+    }
+
+    public static Lexeme parseLine(String line) {
+        line = line.replaceAll("^ +| +$|( )+", "$1");
+        String beginPartOfStr = line.length() > 4 ? line.substring(0, 5) : line.substring(0, 3);
+        TypeLexemes type;
+        String name = "";
+        String content = "";
+        if (beginPartOfStr.contains("if ")) {
+            type = TypeLexemes.conIf;
+            name = "if";
+            content = line.substring(3);
+        } else if (beginPartOfStr.contains("els")) {
+            type = TypeLexemes.conEl;
+        } else if (beginPartOfStr.contains("in ")) {
+            type = TypeLexemes.in;
+            content = line.substring(3);
+        } else if (beginPartOfStr.contains("out ")) {
+            type = TypeLexemes.out;
+            content = line.substring(4);
+        } else {
+//            int indexOfSignEqual = line.indexOf('=');
+            if (line.contains("\"")) {
+                type = TypeLexemes.str;
+            } else {
+                type = TypeLexemes.num;
+
+            }
+            name = line.substring(0, line.indexOf('=') - 1);
+            content = line.substring(line.indexOf('=') + 2);
+        }
+        return new Lexeme(type, name, content);
+    }
+
+    public static boolean openFile(String path) {
+        File file = new File(path);
+
+        try {
+            scanner = new Scanner(file);
+            return true;
+        } catch (FileNotFoundException | NoSuchElementException e) {
+            System.out.println("File doesn't exist.");
+            return false;
+        }
+    }
+
+    public static String readOneLine(Scanner scanner) {
+        String oneCodeLine = "";
+        // считаем сначала первую строку
+        oneCodeLine = scanner.nextLine();
+        return oneCodeLine;
+    }
+}
+
+class Lexeme {
+    TypeLexemes type;
+    String name;
+    String content;
+
+    public Lexeme(TypeLexemes type, String name, String content) {
+        this.type = type;
+        this.name = name;
+        this.content = content;
+    }
+}
+
+enum TypeLexemes {
+    num, str, conIf, conEl, in, out
+}
+
+
+
+
+
+
+
+
+
